9efff8ba3264bf725c03e6b87453a130cc7488ae
Orlando Sergio Dominguez <serggio@msn.com>
2017-11-24 12:10:12 -0300

    0.0.2: Pruebas.

--- routes/router.js
+++ routes/router.js
@@ -1,32 +1,57 @@
 var exprss = require('express');
 var router = exprss.Router();
 var uniqid = require('uniqid');
-var fileys = require('fs');
+var fileSys = require('fs');
 var htmlPdf= require('html-pdf');
 var libPath= require('path');
 
 router.get('/', (rq, rs, n)=>{
-  rs.send('Hola mundo desde GET');
+  rs.send('Impresora PDF');
 });
 
-router.get('/download/:pdffile',(rq,rs,n)=>{
+router.get('/download/:pdffile/:pdfname?',(rq,rs,n)=>{
   file = libPath.join(__dirname,'../download/' + rq.params.pdffile);
-  rs.sendFile(file);
+  name = rq.params.pdfname;
+  if(!name) name = 'nombre_del_archivo';
+  rs.set('Content-Type', 'application/force-download'); 
+  rs.set('Content-Type','application/download');
+  rs.set('Content-Disposition', 'attachment; filename=' + name + '.pdf');
+  rs.set('Content-Transfer-Encoding', 'binary');
+  rs.sendFile(file,null,(e)=>{
+    if(e){
+      rs.status(404).end();
+      console.log('No se ha descargado el archivo ' + file + '.');
+    }
+    /*if(!e) fileSys.unlink(file,()=>{
+      console.log('Se ha descargado el archivo ' + file + '.');  
+    });*/
+  });
 });
 
 router.post('/', (rq, rs, n)=>{
-  json = {};
-  file = 'download/' + uniqid() + '.pdf';
+  file = 'download/' + uniqid();
   html = rq.body;
-  htmlPdf
-    .create(html,{formar:'A4'})
-    .toFile(libPath.join(__dirname,'/../',file),function(e,r){
-
-      if(e===null) json = {result:true,rows:'http:\/\/' + rq.headers.host + '/' +file};
-      else json = {result:false,rows:'No se pudo crear el archivo pdf.'};
+  if(typeof(html)!='string'){
+    json = {result:false,rows:'No se pudo crear el archivo pdf.'};
+    rs.send(json);
+  }
+  if(typeof(html)==='string'){
+    config = {
+      format: 'A4',
+      orientation: 'portrait',
+      width: '21cm',
+      height: '29.7cm'
+    };
+    htmlPdf
+    .create(html,config)
+    .toFile(libPath.join(__dirname,'/../',file),(e,r)=>{
+      if(!e) json = {result:true,rows:'http:\/\/' + rq.headers.host + '/' + file};
+      if(e)  json = {result:false,rows:'No se pudo crear el archivo pdf.'};
       rs.send(json);
-
     });
+  }
 });
+
+
 
 module.exports = router;
