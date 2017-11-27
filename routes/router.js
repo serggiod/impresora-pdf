d56a0eed42d5606f2d82e0c9b44566d2366cddef
Orlando Sergio Dominguez <serggio@msn.com>
2017-11-27 07:07:33 -0300

    0.0.3: Pruebas 2.

--- routes/router.js
+++ routes/router.js
@@ -10,26 +10,26 @@ router.get('/', (rq, rs, n)=>{
 });
 
 router.get('/download/:pdffile/:pdfname?',(rq,rs,n)=>{
-  file = libPath.join(__dirname,'../download/' + rq.params.pdffile);
+  file = libPath.join(__dirname,'../download/' + rq.params.pdffile + '.pdf');
   name = rq.params.pdfname;
-  if(!name) name = 'nombre_del_archivo';
+  if(!name) name = 'file-name.pdf';
   rs.set('Content-Type', 'application/force-download'); 
   rs.set('Content-Type','application/download');
-  rs.set('Content-Disposition', 'attachment; filename=' + name + '.pdf');
+  rs.set('Content-Disposition', 'attachment; filename=' + name);
   rs.set('Content-Transfer-Encoding', 'binary');
   rs.sendFile(file,null,(e)=>{
     if(e){
       rs.status(404).end();
       console.log('No se ha descargado el archivo ' + file + '.');
     }
-    /*if(!e) fileSys.unlink(file,()=>{
+    if(!e) fileSys.unlink(file,()=>{
       console.log('Se ha descargado el archivo ' + file + '.');  
-    });*/
+    });
   });
 });
 
 router.post('/', (rq, rs, n)=>{
-  file = 'download/' + uniqid();
+  file = 'download/pdf' + uniqid();
   html = rq.body;
   if(typeof(html)!='string'){
     json = {result:false,rows:'No se pudo crear el archivo pdf.'};
@@ -40,13 +40,19 @@ router.post('/', (rq, rs, n)=>{
       format: 'A4',
       orientation: 'portrait',
       width: '21cm',
-      height: '29.7cm'
+      height: '29.7cm',
+      border: {
+        top:   '0.8cm',
+        right: '0.8cm',
+        bottom:'0.8cm',
+        left:  '0.8cm'
+      },
     };
     htmlPdf
     .create(html,config)
-    .toFile(libPath.join(__dirname,'/../',file),(e,r)=>{
-      if(!e) json = {result:true,rows:'http:\/\/' + rq.headers.host + '/' + file};
-      if(e)  json = {result:false,rows:'No se pudo crear el archivo pdf.'};
+    .toFile(libPath.join(__dirname,'/../',file + '.pdf'),(e,r)=>{
+      if(e===null) json = {result:true,rows:'http:\/\/' + rq.headers.host + '/' + file + '/file-name.pdf'};
+      else json = {result:false,rows:'No se pudo crear el archivo pdf.'};
       rs.send(json);
     });
   }
